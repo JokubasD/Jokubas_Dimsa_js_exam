cc1338ba6890c3c5dd430a2b2b1b3503bcb4241e
JokubasD <jokubasdimsa@gmail.com>
2022-11-15 10:13:29 +0200

    Task 9 done

--- Task 9/script.js
+++ Task 9/script.js
@@ -1,3 +1,5 @@
+'use strict';
+console.log('script.js');
 /* ------------------------------ TASK 9 ---------------------------------------------------------------
 Sukurkite klasę "Movie" (naudokte ES6), kuri gebės sukurti objektus 3 savybėm ir 1 metodu.
 
@@ -9,3 +11,21 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(argTitle, argDirector, argBudget) {
+    this.title = argTitle;
+    this.director = argDirector;
+    this.budget = argBudget;
+  }
+
+  wasExpensive() {
+    return this.budget > 100000000 ? true : false;
+  }
+}
+
+//Test code
+const m1 = new Movie("Titanic", "John", 1200000000);
+const m2 = new Movie("Breaking Bad", "Jesse", 8000000);
+console.log("m1.wasExpensive() ===", m1.wasExpensive());
+console.log("m2.wasExpensive() ===", m2.wasExpensive());
