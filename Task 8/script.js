b60b8c535b8ba687ca12a19efadb01d74dab3e85
JokubasD <jokubasdimsa@gmail.com>
2022-11-15 10:08:03 +0200

    Task 8 done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+  sum(a, b) {
+    return (a + b).toFixed(2);
+  }
+  subtraction(a, b) {
+    return (a - b).toFixed(2);
+  }
+  multiplication(a, b) {
+    return (a * b).toFixed(2);
+  }
+  division(a, b) {
+    return (a / b).toFixed(2);
+  }
+}
+
+//Test code
+const c1 = new Calculator();
+let a = 10;
+let b = 15;
+console.log("c1.sum(a, b) ===", c1.sum(a, b));
+console.log("c1.subtraction(a, b) ===", c1.subtraction(a, b));
+console.log("c1.multiplication(a, b) ===", c1.multiplication(a, b));
+console.log("c1.division(a, b) ===", c1.division(a, b));
