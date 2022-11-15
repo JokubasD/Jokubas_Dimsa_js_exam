87bbcf3c8cb7b748224dd3c2c71179c9ea1dc0bd
JokubasD <jokubasdimsa@gmail.com>
2022-11-15 08:54:29 +0200

    Task 2 done

--- Task 2/script.js
+++ Task 2/script.js
@@ -1,5 +1,18 @@
+'use strict';
+console.log('script.js');
 /* ------------------------------ TASK 2 ----------------------------
 Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+const allHTMLEls = {
+    clickBtn: document.getElementById('btn__element'),
+    clickAmt: document.getElementById('btn__state'),
+}
+
+allHTMLEls.clickBtn.addEventListener('click', increment)
+
+function increment() {
+    allHTMLEls.clickAmt.textContent = Number(allHTMLEls.clickAmt.textContent) + 1
+}
